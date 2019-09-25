e778052f2346c7bd456588c8d05ba8e032b0830f
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-09-25 14:59:33 -0500

    Added Player Ship

--- js/app.js
+++ js/app.js
@@ -40,24 +40,33 @@ const game = {
 game.start();
 console.log(game.aliensShip);
 
+const playerShip = {
+	name: 'USS Assembly',
+	hull: '20',
+	firepower: '5',
+	accuracy: '.7'
+}
+
+console.log(playerShip)
 
 class Alien {
-	constructor(num){
+	constructor(alienName){
 		this.hull = Math.floor(Math.random() *(6 - 3 + 1) + 3);
 		this.firepower = Math.floor(Math.random() *(4 - 2 + 1) + 2);
 		this.accuracy = Math.floor(Math.random() * (8 - 6 + 1) + 6) / 10;
+		this.name = alienName
 	}
 }
 
 const alien1 = new Alien('Alien1')
 console.log(alien1);
-const alien2 = new Alien
+const alien2 = new Alien('Alien2')
 console.log(alien2)
-const alien3 = new Alien
+const alien3 = new Alien('Alien3')
 console.log(alien3)
-const alien4 = new Alien
+const alien4 = new Alien('Alien4')
 console.log(alien4)
-const alien5 = new Alien
+const alien5 = new Alien('Alien5')
 console.log(alien5)
-const alien6 = new Alien
+const alien6 = new Alien('Alien6')
 console.log(alien6)
