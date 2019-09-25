735065bd89c5118d724d6093064e505a648acd9d
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-09-25 16:02:14 -0500

    added while clause

--- js/app.js
+++ js/app.js
@@ -20,9 +20,9 @@
 
 const playerShip = {
 	name: 'USS Assembly',
-	hull: '20',
-	firepower: '5',
-	accuracy: '.7'
+	hull: 20,
+	firepower: 5,
+	accuracy: .7
 }
 
 console.log(playerShip)
@@ -51,8 +51,37 @@ const game = {
 		game.aliensArr.push(alien5)
 		const alien6 = new Alien('Alien6')
 		game.aliensArr.push(alien6)
+	},
+	//battle function will choose first alien array to battle player object
+	attack(){
+
+		while(playerShip.hull > 0 && game.aliensArr[0].hull > 0){
+			console.log(playerShip.name + ' attacks alien ship with 5 damage')
+			game.aliensArr[0].hull -= playerShip.firepower
+			if(game.aliensArr[0].hull <= 0){
+				console.log('Alien ship destroyed')
+			} 
+			else playerShip.hull -= game.aliensArr[0].firepower
+				console.log('remaining alien ship health is ',game.aliensArr[0].hull)
+				console.log(game.aliensArr[0].name, ' attacks the player ship')
+				console.log('remaining playerShip hull health', playerShip.hull)
+
+		}
 	}
-}
+
+	//player will attack log hit and value 
+	//if alien survives,alien will log hit and value if alien attack is accurate
+	//repeat process until either player or alien ship dies
+	//if player wins battle give option to continue
+		//if player chooses to continue present alien2 etc
+			//if player defeats all of the aliens game is over player wins
+		//if player chooses to retreat game ends
+	}
 game.start();
 console.log(game.aliensArr)
+game.attack();
+console.log(game.attack)
 
+
+console.log(game.aliensArr[0].firepower)
+//
