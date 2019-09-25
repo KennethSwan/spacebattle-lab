a06921c21e529c20450fca28873dc06f2dcecc1b
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-09-25 12:31:29 -0500

    added more

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 // if ship survives attacks user 
 // if user survives and destroys ship, option to retreat or advance 
 // 
-
+let randomHull = 0;
 const userShip = {
 	name: "USS Assembly",
 	hull: 20,
@@ -21,5 +21,23 @@ const userShip = {
 userShip.attack()
 
 const alienShip = {
+	hull: randomHull,
+	//firepower: randomFirepower,
+	//accuracy: randAccuracy,
 
 }
+
+const randomStats = (max, min) => {
+
+return	Math.floor(Math.random() *(max - min+ 1)+ min)
+	
+}
+console.log(randomStats(6, 3));
+
+
+
+
+
+
+
+
