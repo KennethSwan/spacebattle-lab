d812da559978ce3e668d5f3aeedfe02e7f439fff
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-09-25 12:16:15 -0500

    created objects

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// need an object for user
+// object for alien ship
+// object for game 
+// attack first
+// if ship survies attacks
+// if user survies attack ship
+// if ship survives attacks user 
+// if user survives and destroys ship, option to retreat or advance 
+// 
+
+const userShip = {
+	name: "USS Assembly",
+	hull: 20,
+	firepower: 5,
+	accuracy: .7,	
+    attack(){
+		console.log("attack");
+	} 
+
+}
+userShip.attack()
+
+const alienShip = {
+
+}
