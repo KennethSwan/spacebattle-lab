534330d3b7668049cd8ccb956f95aed9827c7f17
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-09-25 16:07:41 -0500

    added if else..

--- js/app.js
+++ js/app.js
@@ -58,17 +58,18 @@ const game = {
 		while(playerShip.hull > 0 && game.aliensArr[0].hull > 0){
 			console.log(playerShip.name + ' attacks alien ship with 5 damage')
 			game.aliensArr[0].hull -= playerShip.firepower
-			if(game.aliensArr[0].hull <= 0){
-				console.log('Alien ship destroyed')
-			} 
-			else playerShip.hull -= game.aliensArr[0].firepower
+
+
+			if(game.aliensArr[0].hull > 0) {
+				playerShip.hull -= game.aliensArr[0].firepower
 				console.log('remaining alien ship health is ',game.aliensArr[0].hull)
 				console.log(game.aliensArr[0].name, ' attacks the player ship')
 				console.log('remaining playerShip hull health', playerShip.hull)
-
+			} 
+			else console.log("alien ship destroyed");
 		}
 	}
-
+}
 	//player will attack log hit and value 
 	//if alien survives,alien will log hit and value if alien attack is accurate
 	//repeat process until either player or alien ship dies
@@ -76,12 +77,9 @@ const game = {
 		//if player chooses to continue present alien2 etc
 			//if player defeats all of the aliens game is over player wins
 		//if player chooses to retreat game ends
-	}
+	
 game.start();
 console.log(game.aliensArr)
 game.attack();
-console.log(game.attack)
 
-
-console.log(game.aliensArr[0].firepower)
 //
